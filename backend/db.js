45d50878065f1a21032c5913abf04975695973fc
NishantKumar-007 <ursnishant4u@gmail.com>
2024-01-04 01:06:03 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  "mongodb+srv://conengineer007:KvEn4Hj8PCSbM4tS@demo.ggplfkr.mongodb.net/todos"
-);
+mongoose.connect("your_URL_here");
 const todoSchema = mongoose.Schema({
   title: String,
   description: String,
